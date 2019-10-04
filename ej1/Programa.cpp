0ccfebb57ea77aa320f619d1a7a6064bf6d2016c
Luis Rebolledo <lrebolledo30@gmail.com>
2019-10-04 11:56:53 -0300

    cambios minimos

--- ej1/Programa.cpp
+++ ej1/Programa.cpp
@@ -11,38 +11,45 @@ Nodo *crearNodo(int n){
 	Nodo *nuevoNodo = new Nodo();
 	
 	nuevoNodo->dato = n;
-	nuevoNodo->der = NULm
+	nuevoNodo->der = NULL;
 	nuevoNodo->izq = NULL;
 	
 	return nuevoNodo;
 }
 
-void insertarNodo(Nodo *arbol, int n){
-	if(arbol == NULL){
-		Nodo nuevoNodo = crearNodo(n);
-		arbol = nuevoNodo;
+insertarNodo(Nodo q, int n){
+	if(q == NULL){
+		Nodo *nuevoNodo = crearNodo(n);
+		q = nuevoNodo;
 	}else{
-		int valorRaiz = arbol->dato;
+		int valorRaiz = q->dato;
 		
 		if(n < valorRaiz){
-			insertarNodo(arbol->izq, n);
+			insertarNodo(q->izq, n);
 		}else{
-			insertarNodo(arbol->der, n);		
+			insertarNodo(q->der, n);		
 		}
 	}
 }
-
-void mostrarArbol(Nodo *arbol, int cont){
-	if(arbol == NULL){
+void mostrarArbol(Nodo *q, int cont){
+	if(q == NULL){
 		return;
 	}else{
-		mostrarArbol(arbol->
-	
-
-
-
+		mostrarArbol(q->der, cont+1);
+		for(int i=0; i<cont; i++){
+			cout<<"  ";
+		}
+		cout<< q->dato <<endl;
+		mostrarArbol(q->izq, cont+1);
+	}
+}
 int main (void) {
+	int cont = 0;
 	Nodo *arbol = NULL;
-	insertarNodo(arbol, 0);
+	arbol = crearNodo(20);	
+	insertarNodo(arbol, 10);
+	insertarNodo(arbol, 99);
+	mostrarArbol(arbol, cont);
+	
     return 0;
 }
